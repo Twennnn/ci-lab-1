600f5a1381182e3477b0437b51c8ea9a102b01c4
Popovich Paul <pa.popovish@gmail.com>
2026-09-25 19:24:54 +0700

    Revert "Experiment: break quality gate"
    This reverts commit 5bf40456f863133b5834e15ca67984dafbc887c2.

--- src/app.js
+++ src/app.js
@@ -5,9 +5,4 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
-// мершрут без теста покрытие <80%
-app.get('/new-endpoint', (req, res) => {
-  res.json({ message: 'hello' });
-});
-
 module.exports = app;
